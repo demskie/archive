521109844567b0673a5d80c5af2c376301b2f26c
Alex Demskie <alex.demskie@bigfishgames.com>
2018-12-11 18:34:37 -0800

    add recursive file archiver

--- archive.go
+++ archive.go
@@ -8,17 +8,107 @@ import (
 	"io"
 	"io/ioutil"
 	"os"
+	"path/filepath"
+	"regexp"
 	"strings"
 	"sync"
 	"time"
+
+	"gopkg.in/kothar/brotli-go.v0/enc"
 )
+
+// package level errors
+var (
+	ErrPathIsNotDirectory       = errors.New("path is not a directory")
+	ErrArchiverHasBeenDestroyed = errors.New("archiver has been destroyed")
+	ErrNothingToArchive         = errors.New("nothing to archive")
+)
+
+func CompressWebserverFiles(dir string) ([]string, error) {
+	return CompressFiles(dir, regexp.MustCompile(strings.Join(
+		[]string{"js", "css", "html", "json", "ico", "eot", "otf", "ttf", "wof"}, "$|")+"$",
+	))
+}
+
+// CompressFiles recursively zips of all regex matched files in a given directory structure
+func CompressFiles(dir string, rgx *regexp.Regexp) ([]string, error) {
+	fileInfo, err := os.Stat(filepath.Clean(dir))
+	if err != nil {
+		return nil, err
+	} else if !fileInfo.IsDir() {
+		return nil, ErrPathIsNotDirectory
+	}
+	matchedFiles := []string{}
+	err = filepath.Walk(dir, func(path string, fileInfo os.FileInfo, err error) error {
+		if err == nil && !fileInfo.IsDir() && rgx.FindString(fileInfo.Name()) != "" {
+			matchedFiles = append(matchedFiles, path)
+			if !strings.HasSuffix(path, ".gz") {
+				_, err = os.Stat(filepath.Join(path, ".gz"))
+				if err != nil {
+					return err
+				}
+				if os.IsNotExist(err) {
+					inputFile, err := os.Open(path)
+					if err != nil {
+						return err
+					}
+					outputFile, err := os.Create(filepath.Join(path, ".gz"))
+					if err != nil {
+						return err
+					}
+					defer outputFile.Close()
+					gzw, err := gzip.NewWriterLevel(outputFile, gzip.BestCompression)
+					if err != nil {
+						return err
+					}
+					defer gzw.Close()
+					_, err = io.Copy(gzw, inputFile)
+					if err != io.EOF {
+						return err
+					}
+				}
+			}
+			if !strings.HasSuffix(path, ".br") {
+				_, err = os.Stat(filepath.Join(path, ".br"))
+				if err != nil {
+					return err
+				}
+				if os.IsNotExist(err) {
+					inputFile, err := os.Open(path)
+					if err != nil {
+						return err
+					}
+					outputFile, err := os.Create(filepath.Join(path, ".br"))
+					if err != nil {
+						return err
+					}
+					defer outputFile.Close()
+					params := enc.NewBrotliParams()
+					if regexp.MustCompile(".js$|.css$|.html$|.json$|.ico$").FindString(fileInfo.Name()) != "" {
+						params.SetMode(enc.TEXT)
+					} else if regexp.MustCompile(".eot$|.otf$|.ttf$|.woff$").FindString(fileInfo.Name()) != "" {
+						params.SetMode(enc.FONT)
+					}
+					btw := enc.NewBrotliWriter(params, outputFile)
+					defer btw.Close()
+					_, err = io.Copy(btw, inputFile)
+					if err != io.EOF {
+						return err
+					}
+				}
+			}
+		}
+		return nil
+	})
+	return matchedFiles, err
+}
 
 type tempFile struct {
 	name   string
 	object *os.File
 }
 
-// Archiver is used to create tar.gz archives from objects in memory
+// Archiver is used to create tar.gz archives
 type Archiver struct {
 	mtx         *sync.Mutex
 	filelist    []tempFile
@@ -66,7 +156,7 @@ func (a *Archiver) AddCSV(filename string, lines [][]string) error {
 	defer a.mtx.Unlock()
 	// ensure that the archiver is still valid
 	if a.isDestroyed() {
-		return errors.New("Archiver has been destroyed")
+		return ErrArchiverHasBeenDestroyed
 	}
 	// create a temporary file
 	file, err := ioutil.TempFile("", "go_archiver_")
@@ -93,16 +183,16 @@ func (a *Archiver) AddCSV(filename string, lines [][]string) error {
 }
 
 // CreateArchive moves all pending temporary files into a tar.gz
-func (a *Archiver) CreateArchive(filepath string) error {
+func (a *Archiver) CreateArchive(path string) error {
 	a.mtx.Lock()
 	defer a.mtx.Unlock()
 	// no need to continue if there is nothing to archive
 	if len(a.filelist) == 0 {
-		return errors.New("nothing to archive")
+		return ErrNothingToArchive
 	}
 	// create an empty tar.gz file
-	filepath = strings.Split(filepath, ".")[0] + ".tar.gz"
-	outputFile, err := os.Create(filepath)
+	path = strings.Split(path, ".")[0] + ".tar.gz"
+	outputFile, err := os.Create(path)
 	if err != nil {
 		return err
 	}
